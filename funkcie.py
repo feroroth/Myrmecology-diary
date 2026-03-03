d50fc26cfd20dd76941cabebcdc4a1c0cdf50f62
feroroth <jakubfrantisekroth@gmail.com>
2026-03-03 18:25:34 +0100

    setting git

---
+++ funkcie.py
@@ -0,0 +1,52 @@
+import tkinter as tk
+
+#Funkcia na načítanie textu z textového súboru
+def read_text(y):
+    with open("text.txt") as file:
+        text = file.read()
+    y.create_text(50,30, text=text)
+
+# Funkcia na zapísanie inputu z pola
+def write_text(x):
+    text = x.get()
+    with open("text.txt", "a", encoding = "utf-8") as file:
+        file.write(text+"\n")
+
+# Funkcia na vyčistenie súboru
+def clean():
+    open("text.txt", "w", encoding = "utf-8")
+
+# Funkcia na porovnanie text.txt s databázou
+def compare():
+
+    # Nové okno pre funkciu
+    ro = tk.Tk()
+    ro.title("Porovnanie")
+    ro.geometry("150x300")
+
+    # vytvoríme canvas
+    c = tk.Canvas(ro, width=150, height=300, bg="white")
+    c.pack()
+
+    # Hlavný kód funkcie
+    with open("databaza.txt") as file:
+        with open("text.txt") as f:
+            text_data = f.readlines()
+            counter2 = 10
+            for druh in file.readlines():
+                druh = druh.strip()
+                counter = 0
+                for y in text_data:
+                    if str(druh.lower().replace(" ", "")) == str(y.strip().lower().replace(" ", "")):
+                        counter += 1
+                if counter != 0:
+                    c.create_text(75, counter2, text = druh+": "+str(counter))
+                    counter2 += 13
+
+    c.mainloop()
+    ro.mainloop()
+
+# Funkcia na vyčistenie plátna
+def reset(y):
+    y.delete("all")
+    y.create_text(150, 10, text="Ahoj!")
